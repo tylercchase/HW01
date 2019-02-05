92831dccc012ac6b79b9552117c74e5638363ba2
Tyler Chase <tylercchase@gmail.com>
2019-02-05 14:28:04 -0800

    final

--- greatest/greatest.cpp
+++ greatest/greatest.cpp
@@ -4,11 +4,14 @@
 * Teacher: John Quan
 * Class: CS201
 * Assignment: greatest Part A
+* Filename: greatest.cpp
+* Purpose: Take numbers from a user and print out the greatest at the end
 * Date: 1/28/2019
 */
 using std::endl;
 using std::cin;
 using std::cout;
+int getInt();
 
 int main() {
 
@@ -25,8 +28,7 @@ int main() {
 
             int userInput;
 
-            cout << "Enter a positive number: ";
-            cin >> userInput;
+            userInput = getInt();
 
             //See if input is valid, and also if input is greater than the greatest number entered
 
@@ -53,4 +55,28 @@ int main() {
         }
     }
     return 0;
+}
+
+int getInt() {
+    int output;
+    //Loops until the user enters a valid input; Returns error if non-int input
+    while (true) {
+        cout << "Enter a positive number(0 to end): ";
+
+        cin >> output;
+
+        if(cin.fail()){
+
+            cout << "Please enter an actual number" << endl;
+
+            //clears the input and ignores what was entered previously, prevents endless loops
+            cin.clear();
+
+            //Ignores until it reaches 999 characters, or until it reaches a new line(end of input)
+            cin.ignore(999,'\n');
+
+        } else
+            break;
+    }
+    return output;
 }
